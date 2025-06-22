1eb23a5369a3237cdcc4a22f91361436aa59bb2e
laith977 <lith.adnan@yahoo.com>
2025-06-22 22:20:56 +0300

    minor edits

--- utils.js
+++ utils.js
@@ -1,16 +1,15 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const storePath = path.join(__dirname, "store.json");
+const storePath = path.join(__dirname, 'store.json');
 
 let store = require(storePath);
-
 const saveStore = () => {
-  fs.writeFileSync(storePath, JSON.stringify(store, null, 2), "utf-8");
+  fs.writeFileSync(storePath, JSON.stringify(store, null, 2), 'utf-8');
 };
 
 const getNextId = (items) => {
-  return items.reduce((max, item) => Math.max(max, item.id || 0), 0) + 1;
+  return items[items.length - 1]?.id + 1 || 1;
 };
 
 const getStore = () => store;
